db9fcde82f8bcc690716c6b6e311bcae90e051a5
AbdulRehman297 <abdul.rehman@knowatechnologies.com>
2025-03-26 20:49:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,26 +10,25 @@ st.title("Skin Disease Detector")
 uploaded_file = st.file_uploader("Upload a skin image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file:
-    # Save the uploaded file temporarily
+    
     image_path = f"temp_image.{uploaded_file.type.split('/')[1]}"
     with open(image_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-    # Display the uploaded image
+    
     st.image(image_path, caption="Uploaded Image", use_container_width=True)
 
-    # Predict skin disease
+    # Predict disease
     disease, confidence = predict_skin_disease(image_path)
     st.write(f"### Diagnosis: {disease}")
     #st.write(f"**Confidence Level:** {confidence}%")
 
-    # Get treatment recommendations from Llama 3
+    # recommendations
     with st.spinner("Fetching treatment recommendations..."):
         recommendation = get_recommendation(disease)
 
-    # Display AI-generated recommendations
+    
     st.subheader("Treatment Recommendations")
     st.write(recommendation)
 
-    # Clean up temporary image
     os.remove(image_path)
